66c63a189807fe4ece399b76cce9fe30f5967bc8
Moris <moris071651@proton.me>
2024-03-16 05:21:45 +0200

    GG

--- Client/src/pages/ApprovePeople/ApprovePeople.jsx
+++ Client/src/pages/ApprovePeople/ApprovePeople.jsx
@@ -1,142 +1,63 @@
+import { useParams } from "react-router-dom";
+import useToken from "../../hooks/useToken";
+
+
 import "./ApprovePeople.css";
+import { useEffect, useState } from "react";
 
 const ApprovePeople = () => {
+  const { projectID } = useParams();
+
+  const [profiles, setProfiles] = useState([]);
+
+  const { token } = useToken();
+
+  useEffect(() => {
+    fetch("/api/applicants", {
+      method: "GET",
+      headers: {
+        Title: projectID,
+        Authentication: `${token}`,
+      },
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        if (response?.applicants) {
+          setProfiles([...response.applicants]);
+        } else {
+          alert("Already exits such name");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="ApprovePeople">
       <div>
-        <h1>Идея за Хактуес - “айде за пица”!</h1>
+        <h1>{projectID}</h1>
         <div>
-          <div>
-            <h2>Николай Вълчанов</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Tempor ornare venenatis
-              curabitur dui adipiscing. Facilisis id arcu posuere in eu bibendum
-              tortor.Lorem ipsum dolor sit amet consec. Lorem ipsum dolor sit
-              amet consectetur. Tempor ornare venenatis curabitur.
-            </p>
-            <ul>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                App development
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Loving pizza
-              </li>
-            </ul>
+          {profiles.map((profile) => {
             <div>
-              <button>Откажи</button>
-              <button>Приеми</button>
-            </div>
-          </div>
-          <div>
-            <h2>Николай Вълчанов</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Tempor ornare venenatis
-              curabitur dui adipiscing. Facilisis id arcu posuere in eu bibendum
-              tortor.Lorem ipsum dolor sit amet consec. Lorem ipsum dolor sit
-              amet consectetur. Tempor ornare venenatis curabitur.
-            </p>
-            <ul>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                App development
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Loving pizza
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Fortune telling
-              </li>
-            </ul>
-            <div>
-              <button>Откажи</button>
-              <button>Приеми</button>
-            </div>
-          </div>
-          <div>
-            <h2>Николай Вълчанов</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Tempor ornare venenatis
-              curabitur dui adipiscing. Facilisis id arcu posuere in eu bibendum
-              tortor.Lorem ipsum dolor sit amet consec. Lorem ipsum dolor sit
-              amet consectetur. Tempor ornare venenatis curabitur.
-            </p>
-            <ul>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                App development
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Loving pizza
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Fortune telling
-              </li>
-            </ul>
-            <div>
-              <button>Откажи</button>
-              <button>Приеми</button>
-            </div>
-          </div>
-          <div>
-            <h2>Николай Вълчанов</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Tempor ornare venenatis
-              curabitur dui adipiscing. Facilisis id arcu posuere in eu bibendum
-              tortor.Lorem ipsum dolor sit amet consec. Lorem ipsum dolor sit
-              amet consectetur. Tempor ornare venenatis curabitur.
-            </p>
-            <ul>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                App development
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Loving pizza
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Fortune telling
-              </li>
-            </ul>
-            <div>
-              <button>Откажи</button>
-              <button>Приеми</button>
-            </div>
-          </div>
-          <div>
-            <h2>Николай Вълчанов</h2>
-            <p>
-              Lorem ipsum dolor sit amet consectetur. Tempor ornare venenatis
-              curabitur dui adipiscing. Facilisis id arcu posuere in eu bibendum
-              tortor.Lorem ipsum dolor sit amet consec. Lorem ipsum dolor sit
-              amet consectetur. Tempor ornare venenatis curabitur.
-            </p>
-            <ul>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                App development
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Loving pizza
-              </li>
-              <li>
-                <img src="/src/assets/tick.png" alt="" />
-                Fortune telling
-              </li>
-            </ul>
-            <div>
-              <button>Откажи</button>
-              <button>Приеми</button>
-            </div>
-          </div>
+              <h2>{profile.username}</h2>
+              <p>{profile.description}</p>
+              <ul>
+                {profile.skills.map((skill) => {
+                  <li>
+                    <img src="/src/assets/tick.png" alt="" />
+                    {skill}
+                  </li>
+                })}
+              </ul>
+              <div>
+                <button>Откажи</button>
+                <button>Приеми</button>
+              </div>
+            </div>;
+          })}
         </div>
       </div>
       <div>
