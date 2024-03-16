fd75d20064a493f61cf6e85ea285f91b49088287
Moris <moris071651@proton.me>
2024-03-16 05:21:22 +0200

    gg

--- Client/src/pages/CreateProject/CreateProject.jsx
+++ Client/src/pages/CreateProject/CreateProject.jsx
@@ -4,18 +4,18 @@ import useToken from "../../hooks/useToken";
 
 import "./CreateProject.css";
 
-import Modal from 'react-modal';
+import Modal from "react-modal";
 
-Modal.setAppElement('#root');
+Modal.setAppElement("#root");
 const customStyles = {
-    content: {
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
-    },
+  content: {
+    top: "50%",
+    left: "50%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    transform: "translate(-50%, -50%)",
+  },
 };
 
 const CreateProject = () => {
@@ -28,6 +28,8 @@ const CreateProject = () => {
 
   const [modalIsOpen, setIsOpen] = useState(false);
 
+  const [newSkill, setNewSkill] = useState("");
+  const [skills, setSkills] = useState([]);
 
   const sendData = (e) => {
     e.preventDefault();
@@ -35,16 +37,25 @@ const CreateProject = () => {
     fetch("/api/projects", {
       method: "POST",
       headers: {
-        'Content-Type': "application/json",
-        'Authentication': `${token}`,
+        "Content-Type": "application/json",
+        Authentication: `${token}`,
       },
       body: JSON.stringify({
         title: projectName,
-        skills: ["Hello", "Getter", "Setter"],
+        skills: skills,
       }),
     })
       .then((response) => {
-        console.log(response);
+        return response.json()
+      })
+      .then((response) => {
+        console.log(response?.created)
+        if (response?.created == true) {
+          location.href = '/approve/' + projectName
+        }
+        else {
+          alert('Already exits such name')
+        }
       })
       .catch((error) => {
         console.log(error);
@@ -53,68 +64,83 @@ const CreateProject = () => {
 
   return (
     <>
-    <Modal
+      <Modal
         isOpen={modalIsOpen}
         onAfterOpen={() => null}
-        onRequestClose={() => setIsOpen(false)}
+        onRequestClose={() => {
+          setIsOpen(false);
+          if (newSkill !== "") {
+            setSkills([...skills, newSkill]);
+            setNewSkill("");
+          }
+        }}
         style={customStyles}
         contentLabel="Example Modal"
-    >
-        <div onClick={() => setIsOpen(false)}>X</div>
-        <input type="text"/>
+      >
+        <div onClick={() => {
+          setIsOpen(false);
+          if (newSkill !== "") {
+            setSkills([...skills, newSkill]);
+            setNewSkill("");
+          }}}>
+        </div>
+        <input
+          value={newSkill}
+          onChange={(e) => {
+            setNewSkill(e.target.value);
+          }}
+          type="text"
+        />
         <p>Category</p>
-
-    </Modal>
-    <form className="CreateProject" onSubmit={sendData}>
-      <h1>Creating a project</h1>
-      <input
-        className="inputfocus"
-        type="text"
-        value={projectName}
-        onChange={(e) => {
-          setProjectName(e.target.value);
-        }}
-        placeholder="Project Name"
-      />
-      <div>
+      </Modal>
+      <form className="CreateProject" onSubmit={sendData}>
+        <h1>Creating a project</h1>
+        <input
+          className="inputfocus"
+          type="text"
+          value={projectName}
+          onChange={(e) => {
+            setProjectName(e.target.value);
+          }}
+          placeholder="Project Name"
+        />
         <div>
-          <textarea
-            id="ProjectDesc"
-            value={projectDesc}
-            onChange={(e) => {
-              setProjectDesc(e.target.value);
-            }}
-            placeholder="Project Description"
-            className="inputfocus"
-          ></textarea>
-          <textarea
-            id="HelpDesc"
-            value={helpDesc}
-            onChange={(e) => {
-              setHelpDesc(e.target.value);
-            }}
-            placeholder="Team Description"
-            className="inputfocus"
-          ></textarea>
-        </div>
-        <div>
-          <h2>What skills are you searching for?</h2>
-          <div className="SkillSet">
-            <p>App development</p>
-            <p>Figma</p>
-            <p>Pizza eating</p>
-            <p>Webflow</p>
-            <p>Figma</p>
-            <div onClick={() => setIsOpen(true)} className="pointer">+</div>
+          <div>
+            <textarea
+              id="ProjectDesc"
+              value={projectDesc}
+              onChange={(e) => {
+                setProjectDesc(e.target.value);
+              }}
+              placeholder="Project Description"
+              className="inputfocus"
+            ></textarea>
+            <textarea
+              id="HelpDesc"
+              value={helpDesc}
+              onChange={(e) => {
+                setHelpDesc(e.target.value);
+              }}
+              placeholder="Team Description"
+              className="inputfocus"
+            ></textarea>
           </div>
-          <div className="ButtonHighlight">
-          <button type="submit">
-            Create project
-          </button>
+          <div>
+            <h2>What skills are you searching for?</h2>
+            <div className="SkillSet">
+              {skills.map((skill) => {
+                return <p>{skill}</p>;
+              })}
+              <div onClick={() => setIsOpen(true)} className="pointer">
+                +
+              </div>
+            </div>
+            <div className="ButtonHighlight">
+              <button type="submit">Create project</button>
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
     </>
   );
 };
